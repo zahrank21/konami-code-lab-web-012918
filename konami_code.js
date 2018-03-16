fe9b013c8f2880c3c20787eb6141b1b42a362276
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-16 16:20:02 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let idx = 0
 
-}
+
+function init(e){
+document.body.addEventListener('keydown', function(e){
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[idx]){
+    idx++;
+
+    if (idx === code.length){
+      alert("hurray!")
+    }
+
+  }else {
+    idx = 0
+  }
+})}
